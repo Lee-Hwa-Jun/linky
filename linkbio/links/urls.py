2ccd65ae37c5e93418b7d808b5dcf53a411cf34c
이화준 <hjlee0090@douzone.com>
2026-01-27 11:47:06 +0900

    Update urls.py

--- linkbio/links/urls.py
+++ linkbio/links/urls.py
@@ -6,8 +6,9 @@ app_name = "links"
 
 urlpatterns = [
     path("lucky/", views.lucky, name="lucky"),
+    path("", views.lucky, name="lucky"),
     path("lucky/fortune/", views.fortune_status, name="fortune_status"),
-    path("", views.landing, name="landing"),
+    # path("", views.landing, name="landing"),
     path("<slug:slug>/links/", views.profile_links, name="profile_links"),
     path("<slug:slug>/go/<int:link_id>/", views.track_link, name="link_redirect"),
     path("<slug:slug>/", views.landing, name="profile"),
